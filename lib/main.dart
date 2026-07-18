a52176df30890183d47eb47fa4680a3d791e04a6
AskarKasimov <kasimovaskar123@gmail.com>
2026-07-19 01:18:00 +0800

    feat: wire dark theme and persisted theme mode into MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/theme/app_theme.dart';
+import 'core/theme/theme_mode_provider.dart';
 import 'features/daily_cards/presentation/providers/providers.dart';
 import 'features/home/presentation/screens/home_screen.dart';
 
@@ -18,14 +19,16 @@ void main() async {
   );
 }
 
-class LampadaApp extends StatelessWidget {
+class LampadaApp extends ConsumerWidget {
   const LampadaApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Лампада',
       theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ref.watch(themeModeProvider),
       home: const HomeScreen(),
     );
   }
